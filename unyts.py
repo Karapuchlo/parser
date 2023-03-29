960c54259c6da1a9c910b1a2e7e6222873516d77
Aleksey <mc_gra_dy@mail.ru>
2023-03-29 11:35:57 +0300

    поменял ключи в запросе

--- unyts.py
+++ unyts.py
@@ -4,11 +4,11 @@ import csv
 import json
 
 class Vacancy:
-    def __init__(self, id, name, salary_from, salary_to):
-        self.id = id
+    def __init__(self, employer_name, name, salary, alternate_url):
+        self.employer_name = employer_name
         self.name = name
-        self.salary_from = salary_from
-        self.salary_to = salary_to
+        self.salary = salary
+        self.alternate_url = alternate_url
 class HHAPIParser:
 
     def fetch_vacancies(self):
@@ -34,8 +34,8 @@ class HHAPIParser:
         # Обрабатываем полученные данные
         vacancies = []
         for vacancy_data in response.json()['items']:
-            if vacancy_data['salary']['from'] is not None:
-                vacancy = Vacancy(vacancy_data['id'], vacancy_data['name'], vacancy_data['salary']['from'], vacancy_data['salary']['to'])
+            if vacancy_data['salary'] is not None:
+                vacancy = Vacancy(vacancy["name"],vacancy["employer"]["name"], vacancy_data['salary'], vacancy["alternate_url"])
                 vacancies.append(vacancy)
 
         # Возвращаем список вакансий
@@ -50,7 +50,7 @@ class HHAPIExporter:
         # Экспортируем вакансии в CSV-файл
         with open('vacancies.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['ID', 'Название', 'Зарплата с', 'Зарплата до'])
+            writer.writerow(['Название', 'Работодатель', 'Зарплата', 'Ссылка'])
             for vacancy in vacancies:
-                if vacancy.salary_from is not None:
-                    writer.writerow([vacancy.id, vacancy.name, vacancy.salary_from, vacancy.salary_to])
+                if vacancy.salary is not None:
+                    writer.writerow([vacancy.name,  vacancy.salary, vacancy.alternate_url])
