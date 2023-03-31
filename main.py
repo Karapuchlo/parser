5a7ad66073dc6c51f754abbef5304611efb62494
Aleksey <mc_gra_dy@mail.ru>
2023-03-31 17:30:33 +0300

    пытаюсь добавить  меню программы, но пока не очень

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from unyts import HHAPIExporter, fetch_vacancies_hh, SuperjobAPIParser
 from unyts import HHAPIParser, export_vacancies_hh
+import utils
 
 
 def run_parser_hh():
@@ -38,3 +39,42 @@ def run_parser_Superjob():
     print(exporter_Superjob)
     # Экспортируем данные в нужном формате
     export_vacancies_hh(vacancies_Superjob)
+
+
+class SuperjobAPIExporter:
+    pass
+
+
+def main():
+    print('Выберите сайт для получения вакансий:')
+    print('1. HeadHunter')
+    print('2. Superjob')
+    site_choice = input()
+
+    if site_choice == '1':
+        print('Введите параметры для поиска вакансий на HeadHunter:')
+        args = utils.parse_arguments()
+
+        parser = HHAPIParser()
+        vacancies = parser.fetch_vacancies(args.keyword, args.location, args.salary, args.count)
+
+        exporter = HHAPIExporter()
+        exporter.export_vacancies(vacancies)
+
+        print('Данные экспортированы в файл vacancies.csv')
+    elif site_choice == '2':
+        print('Введите параметры для поиска вакансий на Superjob:')
+        args = utils.parse_arguments()
+
+        parser = SuperjobAPIParser()
+        vacancies = parser.fetch_vacancies(args.keyword, args.location, args.salary_from, args.salary_to, args.count)
+
+        exporter = SuperjobAPIExporter()
+        exporter.export_vacancies(vacancies)
+
+        print('Данные экспортированы в файл vacancies.csv')
+    else:
+        print('Неправильный выбор.')
+
+if __name__ == '__main__':
+    main()
